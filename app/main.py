af24746a23a62502a57014a4283dcfed5c4f7206
Omkar Kabde <omkarkabde@gmail.com>
2025-03-14 12:29:57 +0530

    cat-file fix

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def main():
     elif command == "cat-file" and sys.argv[2] == "-p":
         hash = sys.argv[3]
         with open(f".git/objects/{hash[:2]}/{hash[2:]}", "rb") as f:
-            data = zlib.decompress(f.read()).decode()
+            data = zlib.decompress(f.read())
             header, content = data.split(b"\0", maxsplit=1)
             print(content.decode(encoding="utf-8"), end="")
     else:
