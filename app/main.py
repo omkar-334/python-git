0d1d32882f47876272b477f5d2b3aa09a8740060
Omkar Kabde <omkarkabde@gmail.com>
2025-03-14 17:52:16 +0530

    ls-tree fix

--- app/main.py
+++ app/main.py
@@ -128,7 +128,7 @@ def main():
             f.write(data)
         print(hash)
     elif command == "ls-tree":
-        hash = "a012bf7ae0ee68992570d8172871eb86f4b7e96d"
+        hash = sys.argv[3]
         tree = LsTreeModel(hash)
         tree.call(sys.argv[2])
 
