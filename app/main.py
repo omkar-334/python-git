4e15750492aea7ba3130df53eae3957254f0b3e1
Omkar Kabde <omkarkabde@gmail.com>
2025-03-18 23:49:53 +0530

    migrate from os to pathlib

--- app/main.py
+++ app/main.py
@@ -2,26 +2,48 @@ import os
 import sys
 import zlib
 from hashlib import sha1
+from pathlib import Path
 
 from .ls_tree import LsTreeModel
 
 
-def hash_object(obj_type, content):
+def get_hash_path(parent: Path, hash: str):
+    pre = hash[:2]
+    post = hash[2:]
+    p = parent / ".git" / "objects" / pre / post
+    return p
+
+
+def write_object(parent: Path, obj_type: str, content: bytes):
     content = f"{obj_type} {len(content)}\0".encode("utf-8") + content
+    compressed_content = zlib.compress(content, level=zlib.Z_BEST_SPEED)
     hash = sha1(content).hexdigest()
 
-    dir_path = f".git/objects/{hash[:2]}"
-    os.makedirs(dir_path, exist_ok=True)
+    p = get_hash_path(parent, hash)
+    p.parent.mkdir(parents=True, exist_ok=True)
+    p.write_bytes(compressed_content)
 
-    with open(f"{dir_path}/{hash[2:]}", "wb") as f:
-        f.write(zlib.compress(content))
-    return hash
+
+def read_object(parent: Path, hash: str):
+    p = get_hash_path(parent, hash)
+    bs = p.read_bytes()
+    header, content = zlib.decompress(bs).split(b"\0", maxsplit=1)
+    obj_type, _ = header.split(b" ")
+    return obj_type.decode(), content
+
+
+def init_repo(parent: Path):
+    (parent / ".git").mkdir(parents=True)
+    (parent / ".git" / "objects").mkdir(parents=True)
+    (parent / ".git" / "refs").mkdir(parents=True)
+    (parent / ".git" / "refs" / "heads").mkdir(parents=True)
+    (parent / ".git" / "HEAD").write_text("ref: refs/heads/main\n")
 
 
 def write_tree(path: str):
     if os.path.isfile(path):
         content = open(path, "rb").read()
-        return hash_object("blob", content)
+        return write_object("blob", content)
 
     contents = sorted(
         os.listdir(path),
@@ -50,74 +72,38 @@ def write_tree(path: str):
     return hash
 
 
-# case ["commit-tree", tree_sha, "-p", commit_sha, "-m", message]:
-# contents = b"".join(
-#     [
-#         b"tree %b\n" % tree_sha.encode(),
-#         b"parent %b\n" % commit_sha.encode(),
-#         b"author ggzor <30713864+ggzor@users.noreply.github.com> 1714599041 -0600\n",
-#         b"committer ggzor <30713864+ggzor@users.noreply.github.com> 1714599041 -0600\n\n",
-#         message.encode(),
-#         b"\n",
-#     ]
-# )
-# hash = write_object(Path("."), "commit", contents)
-# print(hash)
-
-
 def main():
-    command = sys.argv[1]
-    if command == "init":
-        os.mkdir(".git")
-        os.mkdir(".git/objects")
-        os.mkdir(".git/refs")
-        with open(".git/HEAD", "w") as f:
-            f.write("ref: refs/heads/main\n")
-        print("Initialized git directory")
+    match sys.argv[1]:
+        case ["init"]:
+            init_repo(Path("."))
+            print("Initialized git directory")
 
-    elif command == "cat-file" and sys.argv[2] == "-p":
-        hash = sys.argv[3]
-        with open(f".git/objects/{hash[:2]}/{hash[2:]}", "rb") as f:
-            data = zlib.decompress(f.read())
-            header, content = data.split(b"\0", maxsplit=1)
-            print(content.decode(encoding="utf-8"), end="")
-    elif command == "hash-object" and sys.argv[2] == "-w":
-        path = sys.argv[3]
-        content = open(path, "rb").read()
-        hash = hash_object("blob", content)
-        print(hash)
-    elif command == "ls-tree":
-        hash = sys.argv[3]
-        tree = LsTreeModel(hash)
-        tree.call(sys.argv[2])
+        case ["cat-file", "-p", hash]:
+            _, content = read_object(Path("."), hash)
+            print(content)
 
-    elif command == "write-tree":
-        print(write_tree("./"))
+        case ["hash-object", "-w", path]:
+            hash = write_object(Path("."), "blob", Path(path).read_bytes())
+            print(hash)
 
-    elif command == "commit-tree":
-        tree_hash = sys.argv[2]
-        commit_message = None
-        parent_hash = None
-        if sys.argv[3] == "-m":
-            commit_message = sys.argv[4]
-        if sys.argv[3] == "-p":
-            parent_hash = sys.argv[4]
-            if sys.argv[5] == "-m":
-                commit_message = sys.argv[6]
+        case ["ls-tree", mode, hash]:
+            tree = LsTreeModel(hash)
+            tree.call(mode)
 
-        parent_clause = f"parent {parent_hash}\n" if parent_hash else ""
+        case ["write-tree"]:
+            print(write_tree("./"))
 
-        commit_content = (
-            f"tree {tree_hash}\n{parent_clause}"
-            f"author Omkar Kabde <omkarkabde@gmail.com> 1234567890 -0700\n"
-            f"committer Omkar Kabde <omkarkabde@gmail.com> 1234567890 -0700\n\n"
-            f"{commit_message}\n"
-        )
-        hash = hash_object("commit", commit_content.encode("utf-8"))
-        print(hash)
+        case ["commit-tree", tree_hash, "-p", parent_hash, "-m", message]:
+            parent_clause = f"parent {parent_hash}\n" if parent_hash else ""
 
-    else:
-        raise RuntimeError(f"Unknown command #{command}")
+            commit_content = (
+                f"tree {tree_hash}\n{parent_clause}"
+                f"author Omkar Kabde <omkarkabde@gmail.com> 1234567890 -0700\n"
+                f"committer Omkar Kabde <omkarkabde@gmail.com> 1234567890 -0700\n\n"
+                f"{message}\n"
+            )
+            hash = write_object(Path("."), "commit", commit_content)
+            print(hash)
 
 
 if __name__ == "__main__":
